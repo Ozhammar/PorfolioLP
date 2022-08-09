d9a315427052799b924cfc6c28054b0d77e9abe7
Lucas Povolo <lucas.povolo1@gmail.com>
2022-08-09 00:26:14 -0300

    bg & login css
    agregado de login y routeo

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClientModule } from '@angular/common/http';
     NavegacionComponent,
     ProyectosComponent,
     SkillsComponent,
-    FooterComponent    
+    FooterComponent,
+    HomeComponent,
+    LoginComponent    
   ],
 
   imports: [
