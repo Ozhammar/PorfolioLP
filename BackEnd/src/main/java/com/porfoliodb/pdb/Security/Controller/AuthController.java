5869831057baa66075d6bda1f092aa6255acd919
Lucas Povolo <lucas.povolo1@gmail.com>
2022-08-10 01:10:39 -0300

    SecurityMain Class
    inicializacion y codeo del primer override

---
+++ BackEnd/src/main/java/com/porfoliodb/pdb/Security/Controller/AuthController.java
@@ -0,0 +1,85 @@
+package com.porfoliodb.pdb.Security.Controller;
+
+import com.porfoliodb.pdb.Security.Dto.JwtDto;
+import com.porfoliodb.pdb.Security.Dto.LoginUsuario;
+import com.porfoliodb.pdb.Security.Dto.NuevoUsuario;
+import com.porfoliodb.pdb.Security.Entity.Rol;
+import com.porfoliodb.pdb.Security.Entity.Usuario;
+import com.porfoliodb.pdb.Security.Enums.RolNombre;
+import com.porfoliodb.pdb.Security.Service.RolService;
+import com.porfoliodb.pdb.Security.Service.UsuarioService;
+import com.porfoliodb.pdb.Security.jwt.JwtProvider;
+import java.util.HashSet;
+import java.util.Set;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+public class AuthController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UsuarioService usuarioService;
+    @Autowired
+    RolService rolService;
+    @Autowired
+    JwtProvider jwtProvider;
+    
+    @PostMapping("/nuevo")
+    public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Mensaje("Datos Invalidos"), HttpStatus.BAD_REQUEST);
+        
+        if (usuarioService.existsByNombreUsuario(nuevoUsuario.getNombreUsuario()))
+            return new ResponseEntity(new Mensaje("Nombre de usuario existente"), HttpStatus.BAD_REQUEST);
+        
+        if (usuarioService.existsByEmail (nuevoUsuario.getEmail()))
+            return new ResponseEntity(new Mensaje("Email existente"), HttpStatus.BAD_REQUEST);
+        
+        Usuario usuario = new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(), nuevoUsuario.getEmail(), passwordEncoder.encode(nuevoUsuario.getPassword()));
+        
+        Set<Rol> roles = new HashSet<>();
+        roles.add(rolService.getByRolNombre(RolNombre.ROLE_USER).get());
+        
+        if(nuevoUsuario.getRoles().contains("admin"))roles.add(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get());
+        usuario.setRoles(roles);
+        usuarioService.save(usuario);
+        
+        return new ResponseEntity(new Mensaje("Usuario creado exitosamente"), HttpStatus.CREATED);
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Mensaje("Campos incorrectos"), HttpStatus.BAD_REQUEST);
+        
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(), loginUsuario.getPassword()));
+        
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        String jwt = jwtProvider.generateToken(authentication);
+        
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        
+        JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+        return new ResponseEntity(jwtDto, HttpStatus.OK);
+    }
+}
---
+++ BackEnd/src/main/java/com/porfoliodb/pdb/Security/Controller/Mensaje.java
@@ -0,0 +1,25 @@
+package com.porfoliodb.pdb.Security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    
+    //Construct
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
